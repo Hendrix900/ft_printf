b5ca8f44832e778b376340376ace1c159a82b6f5
ccastill <ccastill@student.42madrid.com>
2020-07-09 07:18:04 +0200

    1

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/21 19:48:56 by carlos            #+#    #+#             */
-/*   Updated: 2020/07/09 07:01:30 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/09 07:17:06 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,21 @@ int main ()
     int f;
     int g;
     
-    f = ft_printf("Hola que tal %-60s Aquí seguimos escribiendo %07d\n",c ,a);
-    g = printf("Hola que tal %-60s Aquí seguimos escribiendo %07d\n",c ,a);
+    //f = ft_printf("Hola que tal %-60s Aquí seguimos escribiendo %07d\n",c ,a);
+   // g = printf("Hola que tal %-60s Aquí seguimos escribiendo %07d\n",c ,a);
 
 
      
+	
+	f = ft_printf("El número hexadecimal es : %x\n", a);
+	g = printf("El número hexadecimal es : %x\n", a);
+
     printf("%d\n", f);
     printf("%d\n", g);
 
-	
-	//ft_printf("El número hexadecimal es : %x\n", a);
-	//printf("El número hexadecimal es : %x\n", a);
+    f = ft_printf("%%\n");
+    printf("%d\n", f);
 
-    
     return(0);
 
 }
