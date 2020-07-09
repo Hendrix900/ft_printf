c93b2e1bcbfe9fb469e176dcdf4a54e3c982dcd7
ccastill <ccastill@student.42madrid.com>
2020-07-09 05:29:52 +0200

    1

--- ft_putspace.c
+++ ft_putspace.c
@@ -6,17 +6,17 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 20:32:15 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/09 05:27:27 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/09 05:29:46 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void ft_putspace_left(char *new, t_list_printf *next)
+void	ft_putspace_left(char *new, t_list_printf *next)
 {
-	size_t width;
-	size_t variable;
-	int total;
+	size_t	width;
+	size_t	variable;
+	int		total;
 
 	width = next->width;
 	variable = ft_strlen(new);
@@ -36,11 +36,11 @@ void ft_putspace_left(char *new, t_list_printf *next)
 	}
 }
 
-void ft_putspace_right(char *new, t_list_printf *next)
+void	ft_putspace_right(char *new, t_list_printf *next)
 {
-	size_t width;
-	size_t variable;
-	int total;
+	size_t	width;
+	size_t	variable;
+	int		total;
 
 	width = next->width;
 	variable = ft_strlen(new);
@@ -58,7 +58,7 @@ void ft_putspace_right(char *new, t_list_printf *next)
 	}
 }
 
-void ft_putspace(char *new, t_list_printf *next)
+void	ft_putspace(char *new, t_list_printf *next)
 {
 	if (next->flags == '-')
 		ft_putspace_right(new, next);
