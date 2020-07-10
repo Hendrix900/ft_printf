c853b8d51da0afd97469358f65012ddfefb599bb
ccastill <ccastill@student.42madrid.com>
2020-07-11 01:28:57 +0200

    1

--- ft_width.c
+++ ft_width.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 05:50:04 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/11 01:15:35 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/11 01:28:32 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ int	ft_width(const char *s, t_list_printf *next)
 		next->width = ft_asterisk(next);
 	if (next->punt == '.')
 		next->precision = ft_convert(s, next);
-	printf("el ancho es : %d\n", next->width);
-	printf("La precision es : %d\n", next->precision);
-	printf("LA flag es : %d\n", next->flags);
-	printf("LA PRecision es : %c\n", next->punt);
+//	printf("el ancho es : %d\n", next->width);
+//	printf("La precision es : %d\n", next->precision);
+//	printf("LA flag es : %d\n", next->flags);
+//	printf("LA PRecision es : %c\n", next->punt);
 	
 	return (next->width);
 }
