e3a3dc6cbd9cdb87ff0420eeebe2f088d27a4cb0
ccastill <carlosc90@gmail.com>
2020-07-10 03:24:57 +0200

    1

--- ft_width.c
+++ ft_width.c
@@ -6,7 +6,7 @@
 /*   By: carlos <carlos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 05:50:04 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/10 03:09:42 by carlos           ###   ########.fr       */
+/*   Updated: 2020/07/10 03:23:59 by carlos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ int	ft_conver_width(const char *s, t_list_printf *next)
 int	ft_width(const char *s, t_list_printf *next)
 {
 	int count;
-	printf("len es : %c\n", s[next->len]);
+	//printf("len es : %c\n", s[next->len]);
 
 	if (next->flags == '0' || next->flags == 1 || next->flags == '-')
 		next->width = ft_conver_width(s, next);
@@ -63,10 +63,10 @@ int	ft_width(const char *s, t_list_printf *next)
 	}
 	else if (next->punt == '.')
 		next->precision = ft_conver_width(s, next);
-	printf("len es : %c\n", s[next->len]);
-	printf("tiene el :%c\n", next->punt);
-	printf("EL ancho es :%d\n", next->width);
-	printf("La precisión es %d\n", next->precision);
+	//printf("len es : %c\n", s[next->len]);
+	//printf("tiene el :%c\n", next->punt);
+	//printf("EL ancho es :%d\n", next->width);
+	//printf("La precisión es %d\n", next->precision);
 
 	return (next->width);
 }
