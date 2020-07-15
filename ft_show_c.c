0d9aa54e7691ef1c97a540466fdb46e0bf626a2c
ccastill <ccastill@student.42madrid.com>
2020-07-15 20:20:01 +0200

    1

--- ft_show_c.c
+++ ft_show_c.c
@@ -6,25 +6,22 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 02:16:52 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/15 20:15:42 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/15 20:19:46 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_show_c(const char *s, t_list_printf *next)
+void ft_show_c(const char *s, t_list_printf *next)
 {
 	char c;
 	char *new;
 	c = next->c + '0';
-	new = malloc(sizeof(char));
+	new = malloc(sizeof(char)); // añadir 1 espacio para el NULL ??
 	new[1] = '\0';
 	if (next->c == '\0' && next->type != '%')
-	{
-		next->c = '\0';//ft_putchar_fd(0, 1);
-		//next->br++; // Añadir un contador de posiciones a ft_putchar
-	}
-	else if (next->type == '%') 
+		next->c = '\0';
+	else if (next->type == '%')
 		new[0] = s[next->len];
 	else
 		new[0] = next->c;
@@ -37,7 +34,8 @@ void	ft_show_c(const char *s, t_list_printf *next)
 	else if ((next->flags == '0') || (next->punt == '.'))
 		ft_putzero(new, next);
 	else
-		next->type == 'c' && next->c == '\0' ? ft_putchar_fd(0,1,next) : 
-		ft_putstr_fd(new, 1, next);	free(new);
+		next->type == 'c' && next->c == '\0' ? ft_putchar_fd(0, 1, next) :
+		ft_putstr_fd(new, 1, next);
+	free(new);
 	new = NULL;
 }
