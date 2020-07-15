f6af5848e5871916f0168c161a0cdf04154f6d9f
ccastill <ccastill@student.42madrid.com>
2020-07-15 19:30:50 +0200

    1

--- ft_putspace.c
+++ ft_putspace.c
@@ -6,13 +6,13 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 20:32:15 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/15 19:24:40 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/15 19:30:17 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putspace_left(char *new, t_list_printf *next)
+void	ft_putspace(char *new, t_list_printf *next)
 {
 	size_t	width;
 	size_t	variable;
@@ -21,11 +21,13 @@ void	ft_putspace_left(char *new, t_list_printf *next)
 	width = next->width;
 	variable = ft_strlen(new);
 	if (variable >= width)
+		next->type == 'c' && next->c == '\0' ? ft_putchar_fd(0,1) : 
 		ft_putstr_fd(new, 1, next);
 	else if (variable < width)
 	{
 		total = width - variable;
 		if (next->flags == '-')
+		next->type == 'c' && next->c == '\0' ? ft_putchar_fd(0,1) : 
 		ft_putstr_fd(new, 1, next);
 		while (total > 0)
 		{
@@ -34,37 +36,8 @@ void	ft_putspace_left(char *new, t_list_printf *next)
 			next->br++;
 		}
 		if (next->flags != '-')
+		next->type == 'c' && next->c == '\0' ? ft_putchar_fd(0,1) : 
 		ft_putstr_fd(new, 1, next);
 	}
 }
-/*
-void	ft_putspace_right(char *new, t_list_printf *next)
-{
-	size_t	width;
-	size_t	variable;
-	int		total;
 
-	width = next->width;
-	variable = ft_strlen(new);
-	if (variable >= width)
-		ft_putstr_fd(new, 1, next);
-	else if (variable < width)
-	{
-		total = width - variable;
-		ft_putstr_fd(new, 1, next);
-		while (total > 0)
-		{
-			ft_putchar_fd(' ', 1);
-			total--;
-			next->br++;
-		}
-	}
-}
-*/
-void	ft_putspace(char *new, t_list_printf *next)
-{
-/*	if (next->flags == '-')
-		ft_putspace_right(new, next);
-	else*/
-		ft_putspace_left(new, next);
-}
