13537f0ef17e1cc63f53a2ad67e9aa187f339f14
ccastill <ccastill@student.42madrid.com>
2020-07-15 22:50:48 +0200

    1

--- ft_width.c
+++ ft_width.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 05:50:04 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/15 22:40:58 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/15 22:50:34 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,10 @@ int	ft_width(const char *s, t_list_printf *next)
 		next->precision = ft_convert(s, next);
 	if (next->neg == 1) // Añadido recientemente. cambia la flag si el entero es negativo.
 		next->flags = '-';
-	printf("el ancho es : %d\n", next->width);
-	printf("La precision es : %d\n", next->precision);
-	printf("LA flag es : %d\n", next->flags);
-	printf("LA PRecision es : %c\n", next->punt);
+//	printf("el ancho es : %d\n", next->width);
+//	printf("La precision es : %d\n", next->precision);
+//	printf("LA flag es : %d\n", next->flags);
+//	printf("LA PRecision es : %c\n", next->punt);
 //	printf("LA posición es : %c\n", s[next->len]);
 
 
