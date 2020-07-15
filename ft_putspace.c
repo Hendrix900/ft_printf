2b12469d80a3942f9c27b97f3273205b958a074b
ccastill <ccastill@student.42madrid.com>
2020-07-15 18:48:22 +0200

    1

--- ft_putspace.c
+++ ft_putspace.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 20:32:15 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/15 18:45:41 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/15 18:48:06 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_putspace_left(char *new, t_list_printf *next)
 	variable = ft_strlen(new);
 	if (variable >= width)
 	{
-		next->c == '\0' ? ft_putchar_fd(0,1) : ft_putstr_fd(new, 1, next);
+		ft_putstr_fd(new, 1, next);
 	}
 	else if (variable < width)
 	{
@@ -46,7 +46,7 @@ void	ft_putspace_right(char *new, t_list_printf *next)
 	width = next->width;
 	variable = ft_strlen(new);
 	if (variable >= width)
-		next->c == '\0' ? ft_putchar_fd(0,1) : ft_putstr_fd(new, 1, next);
+		ft_putstr_fd(new, 1, next);
 	else if (variable < width)
 	{
 		total = width - variable;
