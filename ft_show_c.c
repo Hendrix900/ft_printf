89be92941c44d179bebedfee7e45bcda5e6d42d0
ccastill <ccastill@student.42madrid.com>
2020-07-15 19:39:09 +0200

    1

--- ft_show_c.c
+++ ft_show_c.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 02:16:52 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/15 19:37:47 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/15 19:39:00 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_show_c(const char *s, t_list_printf *next)
 	if (next->c == '\0' && next->type != '%')
 	{
 		//ft_putchar_fd(0, 1);
-		next->br++;
+		//next->br++;
 	}
 	if (next->type == '%')
 		new[0] = s[next->len];
