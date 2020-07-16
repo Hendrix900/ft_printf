fe11f8a40c5dd6130b9f1d2b5169a106f5f80132
ccastill <ccastill@student.42madrid.com>
2020-07-16 19:54:33 +0200

    1

--- ft_show_memory.c
+++ ft_show_memory.c
@@ -6,39 +6,27 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 02:19:04 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/16 19:09:34 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/16 19:49:56 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-#include "ft_printf.h"
-/*
-void ft_memory_zero_space(const char *s , t_list_printf *next, char null)
-{
-
-
-
-
-	
-}*/
-
-void	ft_memoryzero(char *new, t_list_printf *next, char *null)
+void ft_memory_zero_space(char *new, t_list_printf *next, char *null)
 {
 	size_t	precision;
+	size_t	width;
 	size_t	variable;
 	int		total;
-
+	
 	precision = next->precision;
-	variable = ft_strlen(new); //+ ft_strlen(null);
+	width = next->width;
+	variable = ft_strlen(new);
 	if (variable >= precision)
 		ft_putstr_fd(ft_strjoin(null, new), 1, next);
 	else if (variable < precision)
 	{
 		total = precision - variable;
-	//	printf ("El total es : %d\n", total);
-	//	printf ("La variable es : %d\n", variable);
-	//	printf("la precision es : %d", precision);
 		next->precision == '.' ? ft_putstr_fd(ft_strjoin(null, new), 1, next) :
 		ft_putstr_fd(null, 1, next);
 		while (total > 0)
@@ -46,6 +34,48 @@ void	ft_memoryzero(char *new, t_list_printf *next, char *null)
 			ft_putchar_fd('0', 1,next);
 			total--;
 		}
+		next->precision == '.' ? ft_putstr_fd(ft_strjoin(null, new), 1, next) :
+		ft_putstr_fd(new, 1, next);
+	}
+}
+
+void	ft_memoryzero(char *new, t_list_printf *next, char *null)
+{
+	size_t	width;
+	size_t	variable;
+	int		total;
+
+	width = next->width;
+	variable = ft_strlen(new) + ft_strlen(null);
+	if (variable >= width)
+		ft_putstr_fd(ft_strjoin(null, new), 1, next);
+	else if (variable < width)
+	{
+		total = width - variable;
+		next->flags != '0' ? ft_putstr_fd(ft_strjoin(null, new), 1, next) :
+		ft_putstr_fd(null, 1, next);
+		ft_zeros(total, next);
+		next->flags != '0' ? ft_putstr_fd(ft_strjoin(null, new), 1, next) :
+		ft_putstr_fd(new, 1, next);
+	}
+}
+
+void	ft_memorypunt(char *new, t_list_printf *next, char *null)
+{
+	size_t	precision;
+	size_t	variable;
+	int		total;
+	
+	precision = next->precision;
+	variable = ft_strlen(new);
+	if (variable >= precision)
+		ft_putstr_fd(ft_strjoin(null, new), 1, next);
+	else if (variable < precision)
+	{
+		total = precision - variable;
+		next->precision == '.' ? ft_putstr_fd(ft_strjoin(null, new), 1, next) :
+		ft_putstr_fd(null, 1, next);
+		ft_zeros(total, next);
 		next->precision == '.' ? ft_putstr_fd(ft_strjoin(null, new), 1, next) :
 		ft_putstr_fd(new, 1, next);
 	}
@@ -66,11 +96,7 @@ void	ft_memoryspace(char *new, t_list_printf *next, char *null)
 		total = width - variable;
 		if (next->flags == '-')
 		ft_putstr_fd(ft_strjoin(null, new), 1, next);
-		while (total > 0)
-		{
-			ft_putchar_fd(' ', 1,next);
-			total--;
-		}
+		ft_spaces(total, next);
 		if (next->flags != '-')
 		ft_putstr_fd(ft_strjoin(null, new), 1, next);
 	}
@@ -85,26 +111,17 @@ void ft_show_memory(const char *s , t_list_printf *next)
 	new = ft_tomemory(next->p);
 	/*if (new == NULL)
 	new = null;*/
-	/*if ((next->p == 0 && next->flags == '-' && next->punt == '.') || 
-		(next->p == 0 && next->punt == '.'))
-	{
-		new[1] = 'x';
-
-	}*/
-	/*
-	else if (next->punt > 1 || next->flags == '0')
-		ft_memoryzero(new, next, null);
-	else if ((next->flags == 1) || (next->flags == '-') || (next->flags == '*'))
-		ft_memoryspace(new, next, null);
 	/*else if (new[0] == '-')
 		next->neg = '-';*/
 	/*if (new == '\0')
-		new = null;
-	/*else if (next->flags >= 1 && next->punt > 1)
-		ft_memory_zero_space(new, next, new);*/
-	if ((next->flags == 1) || (next->flags == '-') || (next->flags == '*')) 
+		new = null;*/
+	if (next->flags >= 1 && next->punt > 1)
+		ft_memory_zero_space(new, next, new);
+	else if ((next->flags == 1) || (next->flags == '-') || (next->flags == '*')) 
 		ft_memoryspace(new, next, null);
-	else if ((next->flags == '0') || (next->punt == '.'))
+	else if (next->punt == '.')
+		ft_memorypunt(new, next, null);
+	else if (next->flags == '0')
 		ft_memoryzero(new, next, null);
 	else
 		ft_putstr_fd(ft_strjoin(null, new), 1, next);
