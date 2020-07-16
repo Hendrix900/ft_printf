d370432202ee965edf38e43e87891fdd0d73343b
ccastill <ccastill@student.42madrid.com>
2020-07-17 01:08:51 +0200

    FT_PUT_SPACE FUNCIONANDO PASA LOS TEST

--- ft_putspace_zero.c
+++ ft_putspace_zero.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 00:21:11 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/17 01:05:59 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/17 01:08:09 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,18 @@ void ft_check_ast(size_t v,size_t p, size_t w, char *new, t_list_printf *next)
 		if (next->neg == '-' && v < w)
 			w--;
 		ft_spaces(w, next);
-		if (next->neg == '-' && w < p && w > p && p != 0)
+		if (next->neg == '-' && w < p && p != 0)
 		{
 			ft_putchar_fd('-', 1, next);
 			p++;
 			count = 1;
 		}
-		/*else if (next->neg == '-' && w > p && p != 0)
+		else if (next->neg == '-' && w > p && p != 0)
 		{
 			ft_putchar_fd('-', 1, next);
 			p++;
 			count = 1;
-		}*/
+		}
 		ft_zeros(p, next);
 		ft_putstr_fd(new + count, 1, next);
 	}
