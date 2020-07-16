bacf95a40d0dc968da2c55a95f408208d0dafc62
ccastill <ccastill@student.42madrid.com>
2020-07-16 20:58:53 +0200

    1

--- ft_putspace_zero.c
+++ ft_putspace_zero.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 00:21:11 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/16 20:42:35 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/16 20:58:04 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ void ft_check_ast(size_t v,size_t p, size_t w, char *new, t_list_printf *next)
 {
 	int count;
 	count = 0;
-	
 	if (next->flags == '-')
 		ft_check_prec(v, p, w, new, next);
 	else
@@ -57,7 +56,7 @@ void ft_check_ast(size_t v,size_t p, size_t w, char *new, t_list_printf *next)
 		else if (next->neg == '-' && w > p)
 		{
 			ft_putchar_fd('-', 1, next);
-			p++;
+			//p++;
 			count = 1;
 		}
 		ft_zeros(p, next);
