29b7407b033d421c343baf03a9f49ac52f88f3ba
ccastill <ccastill@student.42madrid.com>
2020-07-17 01:16:10 +0200

    1

--- ft_putspace_zero.c
+++ ft_putspace_zero.c
@@ -6,37 +6,37 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 00:21:11 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/17 01:08:09 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/17 01:15:20 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void ft_check_prec(size_t v,size_t p, size_t w, char *new, t_list_printf *next)
+void	ft_check_prec(size_t v, size_t p, size_t w, char *new, t_list_printf *next)
 {
 	int count;
 
 	count = 0;
-		if (next->neg == '-' && v < w)
-			w--;
-		if (next->neg == '-' && v > w)
-		{
-			ft_putchar_fd('-', 1, next);
-			p++ ? p > 0 : p--; 
-			count = 1;
-		}
-		else if (next->neg == '-' && w > p)
-		{
-			ft_putchar_fd('-', 1, next);
-			p++ ? p > 0 : p--; 
-			count = 1;
-		}
-		ft_zeros(p, next);
-		ft_putstr_fd(new + count, 1, next);
-		ft_spaces(w, next);
+	if (next->neg == '-' && v < w)
+		w--;
+	if (next->neg == '-' && v > w)
+	{
+		ft_putchar_fd('-', 1, next);
+		p++ ? p > 0 : p--;
+		count = 1;
+	}
+	else if (next->neg == '-' && w > p)
+	{
+		ft_putchar_fd('-', 1, next);
+		p++ ? p > 0 : p--;
+		count = 1;
+	}
+	ft_zeros(p, next);
+	ft_putstr_fd(new + count, 1, next);
+	ft_spaces(w, next);
 }
 
-void ft_check_ast(size_t v,size_t p, size_t w, char *new, t_list_printf *next)
+void	ft_check_ast(size_t v, size_t p, size_t w, char *new, t_list_printf *next)
 {
 	int count;
 
@@ -65,7 +65,7 @@ void ft_check_ast(size_t v,size_t p, size_t w, char *new, t_list_printf *next)
 	}
 }
 
-void ft_putspace_zero(char *new, t_list_printf *next)
+void	ft_putspace_zero(char *new, t_list_printf *next)
 {
 	size_t width;
 	size_t precision;
@@ -80,8 +80,7 @@ void ft_putspace_zero(char *new, t_list_printf *next)
 		ft_putzero(new + count, next);
 	else if (width > precision)
 	{
-		precision = variable > precision ? precision = 0 :
-		precision - variable;
+		precision = variable > precision ? precision = 0 : precision - variable;
 		width = width - (precision + variable);
 		ft_check_ast(variable, precision, width, new, next);
 	}
