87592feaf084a22f1ce41091538516c6e09f11ee
ccastill <ccastill@student.42madrid.com>
2020-07-18 07:43:26 +0200

    CAmbios en ft_putspace

--- ft_putspace_memory.c
+++ ft_putspace_memory.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 00:21:11 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/18 07:23:05 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/18 07:42:45 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_put_w_p(size_t p, size_t w, char *new, t_list_printf *next)
 {
 	int count;
-
+	
 	count = 0;
 	if (next->flags != '-')
 		ft_spaces(w, next);
@@ -33,13 +33,16 @@ void	ft_check_w_p(size_t p, size_t w, char *new, t_list_printf *next)
 {
 	size_t	variable;
 	
-	if (new[0] == '0')
-		variable = 2;
-	else
-		variable = ft_strlen(new);
-	p += 2;
+	variable = ft_strlen(new);
+	if (new[0] != '0')
+		p += 2;
 	if (w == p)
 		ft_putzero(new, next);
+	if (new[0] == '0')
+	{
+		w -= 2;
+		ft_put_w_p(p, w, new, next);
+	}
 	else if (w > p)
 	{
 		if ((variable + 2) > p)
