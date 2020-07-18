6cb2f85f5ac2cad7cfc1e77a4de1a0d525651e33
ccastill <ccastill@student.42madrid.com>
2020-07-19 01:56:09 +0200

    Cambios en width: Anterior comit Funcionando!

--- Desechados/ft_precision.c
+++ Desechados/ft_precision.c
@@ -6,49 +6,48 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/16 05:05:27 by ccastill          #+#    #+#             */
-/*   Updated: 2020/07/19 01:55:33 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/07/17 01:39:25 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-size_t	ft_strlen_str_precision(const char *s, int l)
+size_t	ft_strlen_print_p(const char *s, int l)
 {
-	size_t count;
+	size_t q;
 
-	count = 0;
+	q = 0;
 	while (s[l] >= '0' && s[l] <= '9')
 	{
-		count++;
+		q++;
 		l++;
 	}
-	return (count);
-}
-
-void	ft_asterisk_precision(t_list_printf *next)
-{
-	next->precision = va_arg(next->args, int);
-	if (next->precision < 0)
-		next->punt = 0;
-	next->len++;
+	return (q);
 }
 
 int		ft_precision(const char *s, t_list_printf *next)
 {
-	char	*new;
+	char	*substr;
+	size_t	q;
 
 	if (next->punt == '.')
 	{
 		if (s[next->len] >= '0' && s[next->len] <= '9')
 		{
-			new = ft_substr(s, next->len, ft_strlen_str(s, next->len));
-			next->precision = ft_atoi(new);
-			free(new);
-			new = NULL;
-			next->len += ft_strlen_str(s, next->len);
+			substr = ft_substr(s, next->len,
+					(q = ft_strlen_print_p(s, next->len)));
+			next->precision = ft_atoi(substr);
+			free(substr);
+			substr = NULL;
+			next->len += q;
 		}
 		else if (s[next->len] == '*')
-			ft_asterisk(next);
+		{
+			next->precision = va_arg(next->args, int);
+			if (next->precision < 0)
+				next->punt = 0;
+			next->len++;
+		}
 	}
 	return (next->precision);
 }
